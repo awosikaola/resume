aae6dfbf120f38ee438faf4482e1ee5485fb9207
awosikaola <33780901+awosikaola@users.noreply.github.com>
2020-04-24 05:10:53 -0700

    Add files via upload

---
+++ portfolio/js/script.js
@@ -0,0 +1,11 @@
+(function($){
+    var typed = new Typed('span.txt-rotate', {
+        strings: ["Web Development", "Software Development", "Mobile App Development", "Wordpress Development"],
+        typeSpeed: 100,
+        backSpeed: 100,
+        fadeOut: false,
+        smartBackspace: true,
+        loop: true
+    });
+}
+)(jQuery);
